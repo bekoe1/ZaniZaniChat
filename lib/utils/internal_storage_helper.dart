72b7c8edb9e3ea0486090bfed25db036ad413b5b
bekoe1 <zabulimya@gmail.com>
2024-03-03 12:11:20 +0300

    some refactoring

---
+++ lib/utils/internal_storage_helper.dart
@@ -0,0 +1,20 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefsHelper {
+  static final sessionName = "Session";
+
+  static Future<String?> GetSessionToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(sessionName);
+  }
+
+  static Future<void> DeleteSession() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove(sessionName);
+  }
+
+  static Future<void> SetSessionToken(String value) async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(sessionName, value);
+  }
+}
